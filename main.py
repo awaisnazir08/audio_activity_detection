00164062dbccd992f1c1484d5aaaf2e17102122a
awaisnazir08 <owaisnazir2228@gmail.com>
2024-11-24 12:18:43 +0500

    improve model, create evaluation report

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# from moviepy.editor import VideoFileClip
 from audio_extract import extract_audio
 import os
 import torch
@@ -8,19 +7,21 @@ from utils import video_to_audio, extract_timestamps
 import numpy as np
 
 def main():
+    # initialize and load the saved model
     model = LSTMSoundClassifier(input_size=64, hidden_size=128, num_layers=2, output_size=1)
     model.load_state_dict(torch.load('model.pth', weights_only=True))
     model.eval()
 
+    # specify the path where the test video is to be saved/placed
     root_path = 'test_video'
     video_name = os.listdir(root_path)[0]
     video_path = os.path.join(root_path, video_name)
 
+    # extract audio from video
     video_to_audio(video_path, f'audio_{video_name[5: -4]}.mp3')
 
-
     y, sample_rate = librosa.load(f'audio_{video_name[5: -4]}.mp3', sr=22050, mono=True)
-    hop_length = 512
+    hop_length = 256  # parameter used for training
     mel_spec = librosa.feature.melspectrogram(y=y, sr=sample_rate, n_mels=64, hop_length=hop_length)
     mel_spec = librosa.power_to_db(mel_spec, ref=np.max)
     mel_spec = (mel_spec - mel_spec.mean()) / mel_spec.std()
@@ -32,7 +33,6 @@ def main():
         # print('predictions: ', predictions.shape)
     timestamps = extract_timestamps(predictions, hop_length=hop_length, sr=sample_rate, merge_threshold=3)
     print(timestamps)
-
 
 if __name__ == "__main__": 
     main()
